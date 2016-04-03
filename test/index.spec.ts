5d972791e84c0eaaf7334f93fcce111a5a5f058d
theluk <theluk@gmail.com>
2016-04-03 14:19:04 +0200

    forgot git add

---
+++ test/index.spec.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/main.d.ts"/>
+
+import changesetjs from '../lib/index';
+import {expect} from 'chai';
+
+describe('changesetjs', () => {
+  it('should work', async() => {
+    expect(true).to.equal(true);
+  });
+});
+
